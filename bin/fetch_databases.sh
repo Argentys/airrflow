810d246789b925306631cca3ff6637fdf4379154
igortru <itolstoy@gmail.com>
2025-02-03 19:08:35 -0500

    return back

--- bin/fetch_databases.sh
+++ bin/fetch_databases.sh
@@ -2,7 +2,7 @@
 # Written by Gisela Gabernet and released under the MIT license (2020).
 echo "Fetching databases..."
 
-bash fetch_imgt_new.sh -o imgtdb_base
+bash fetch_imgt.sh -o imgtdb_base
 
 fetch_igblastdb.sh -x -o igblast_base
 
