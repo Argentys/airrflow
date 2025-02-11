cced32c359853d0611f096e05f792760a272297b
Igor Tolstoy <itolstoy@gmail.com>
2025-02-11 17:10:48 -0500

    Update fetch_databases.sh

--- bin/fetch_databases.sh
+++ bin/fetch_databases.sh
@@ -7,7 +7,11 @@ wget https://github.com/Argentys/airrflow/raw/refs/heads/master/aux/imgtdb.tar.g
 tar -xvzf imgtdb.tar.gz
 rm imgtdb.tar.gz
 
-fetch_igblastdb.sh -x -o igblast_base
+#fetch_igblastdb.sh -x -o igblast_base
+wget https://github.com/Argentys/airrflow/raw/refs/heads/master/aux/igblast.tar.gz
+tar -xvzf igblast.tar.gz
+rm igblast.tar.gz
+
 
 imgt2igblast.sh -i ./imgtdb_base -o igblast_base
 
