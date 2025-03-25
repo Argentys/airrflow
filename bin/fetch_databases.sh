eb6fe581ded14fcdfac7a45b2d6b4c3c723ffe9c
igortru <itolstoy@gmail.com>
2025-03-24 20:54:34 -0400

    return

--- bin/fetch_databases.sh
+++ bin/fetch_databases.sh
@@ -2,10 +2,10 @@
 # Written by Gisela Gabernet and released under the MIT license (2020).
 echo "Fetching databases..."
 
-#bash fetch_imgt.sh -o imgtdb_base
-wget https://github.com/Argentys/airrflow/raw/refs/heads/master/aux/imgtdb.tar.gz
-tar -xvzf imgtdb.tar.gz
-rm imgtdb.tar.gz
+bash fetch_imgt.sh -o imgtdb_base
+#wget https://github.com/Argentys/airrflow/raw/refs/heads/master/aux/imgtdb.tar.gz
+#tar -xvzf imgtdb.tar.gz
+#rm imgtdb.tar.gz
 
 #fetch_igblastdb.sh -x -o igblast_base
 wget https://github.com/Argentys/airrflow/raw/refs/heads/master/aux/igblast.tar.gz
