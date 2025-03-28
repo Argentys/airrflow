619b776312124e18414c55c95d6a0d9fbda69854
Igor Tolstoy <itolstoy@gmail.com>
2025-03-28 17:45:14 -0400

    Update addSequenceId.py

--- aux/addSequenceId.py
+++ aux/addSequenceId.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
-import pandas as pd
-import sys
-
 
 import gzip
+import pandas as pd
+import sys
+import json
+import os
+
+# Read from the gzip file
 with gzip.open(sys.argv[1], 'rt') as f:
-    first_line = f.readline().strip()  # Read the first line and strip whitespace
-    print("First line:", first_line)  # Print the first line
+    json_string = f.readline().strip()  # Read the first line and strip whitespace
     df = pd.read_csv(f)
 
+# Fix doubled quotes and remove outer quotes
+json_string = json_string.replace('""', '"')  # Fix doubled quotes if present
+if json_string.startswith('"') and json_string.endswith('"'):
+    json_string = json_string[1:-1]  # Remove outer quotation marks
+
+# Parse JSON with error handling
+data = json.loads(json_string)
+#for key in data:
+#  print(f"{key}: {data[key]}")
+
+df = df.rename(columns={col: col.replace('_heavy', '') for col in df.columns if col.endswith('_heavy')})
+
+# Add sequence_id if not present
 if "sequence_id" not in df.columns:
-   df['sequence_id'] = range(1, len(df) + 1)
-nm = sys.argv[1].split(".")[0] + ".tsv"
-df.to_csv(nm, sep='\t', index=False)
+    df['sequence_id'] = range(1, len(df) + 1)
+
+# Save to TSV
+nm= sys.argv[1].split(".")[0] + ".nextflow.dir"
+import shutil
+shutil.rmtree(nm, ignore_errors=True)
+os.mkdir(nm)
+df.to_csv(nm + "/data.tsv", sep='\t', index=False)
+
+ff = open(nm + "/input.tsv","w")
+bt = data['BType']
+bt = bt.replace("/","_")
+defline = f"filename\tspecies\tsubject_id\tsample_id\ttissue\tsex\tage\tbiomaterial_provider\tpcr_target_locus\tsingle_cell";
+meta = f"data.tsv\t{data['Species']}\t{data['Subject']}\t{bt}\t{data['BSource']}\tmale\t{data['Age']}\t{data['Author']}\tIG\tFALSE"
+print(defline, file = ff)
+print(meta, file = ff)
+ff.close()
